d10ef52f3a5afa7771d6d69d3d824c33b7a89194
Samantha Burke <samanthaburke.uk@gmail.com>
2019-11-22 12:06:34 +0000

    pair programming work at end of intensive week

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -1,5 +1,4 @@
 const catalogueService = require("../app/catalogue_service");
-
 describe("catalogueService", () => {
   describe("catalogueService.countBooksByAuthor", () => {
     test("returns the total number of books written by the given author", () => {
@@ -8,4 +7,30 @@ describe("catalogueService", () => {
       expect(catalogueService.countBooksByAuthor("Charles Dickens")).toBe(3);
     });
   });
+  describe("catalogue.Service.checkBookByTitle",() => {
+    test("returns true if the book title exists in the catalogue", () => {
+      expect(catalogueService.checkBookByTitle("The Origin of Species")).toBe(true);
+      expect(catalogueService.checkBookByTitle("Sapiens")).toBe(false);
+    });
+  });
+  describe("catalogue.Service.countBooksByFirstLetter",() => {
+    test("returns the the total number of books beginning with a letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("W")).toBe(2);
+      expect(catalogueService.countBooksByFirstLetter("w")).toBe(2);
+  });
+});
+describe("catalogue.Service.getQuantity",() => {
+  test("returns the quantity of items in stock by given title", () => {
+    expect(catalogueService.getQuantity("Wolf Hall")).toBe(33);
+  });  
+});
+describe("catalogue.getBooksByAuthor",() => {
+  test("returns the array of books by given author", () => {
+    expect(catalogueService.getBooksByAuthor("Charles Dickens")).toBe([
+      {title: "A Tale of Two Cities", author: "Charles Dickens", quantity: 3},
+        {title: "Oliver Twist", author: "Charles Dickens", quantity: 7},
+        {title: "Great Expectations", author: "Charles Dickens", quantity: 1},
+      ]);
+    });
+});
 });
